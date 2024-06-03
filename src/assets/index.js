634b58f644a14c630180fede9e8e53a5ca048a75
Luca Donadello <45766869+LucaDonadello@users.noreply.github.com>
2024-06-03 11:58:15 -0500

    updates
    Changed all the info.

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import aws from "./company/aws.png";
+import python from "./company/python.png";
+import Ai from "./company/Ai.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +51,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  aws,
+  python,
+  Ai,
   carrent,
   jobit,
   tripguide,
