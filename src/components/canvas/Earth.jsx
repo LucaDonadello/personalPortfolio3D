61ab49fdc62928f2de07db38356300c3dfcb349a
Luca Donadello <45766869+LucaDonadello@users.noreply.github.com>
2024-06-05 17:59:00 -0500

    updates

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,16 +5,10 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-
   const earth = useGLTF("./planet/scene.gltf");
   return (
-    <primitive 
-       object={earth.scene}
-        scale={2.5}
-        position-y={0}
-        rotation-y={0}
-       />
-  )
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
 };
 
 const EarthCanvas = () => {
@@ -23,7 +17,12 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
