0b0e0440ed5d57992a003445951d1ee83b85bca3
Luca Donadello <45766869+LucaDonadello@users.noreply.github.com>
2024-06-11 11:31:58 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
         output:{
             manualChunks(id) {
-                if (id.includes('node_modules') || id.includes('assets') ){
+                if (id.includes('node_modules')) {
                     return id.toString().split('node_modules/')[1].split('/')[0].toString();
                 }
             }
