09a7942be0244fdb9f261700b9e71bd353774719
Luca Donadello <45766869+LucaDonadello@users.noreply.github.com>
2024-06-11 16:50:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pages/dist/',
+  base: "https://lucadonadello.github.io/personalPortfolio3D/",
   plugins: [react()],
 })
