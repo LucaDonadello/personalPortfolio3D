daf5c603bb588cccc6b1206082265269c9e72022
Luca Donadello <45766869+LucaDonadello@users.noreply.github.com>
2024-06-11 12:05:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  chunkSizeWarningLimit:1500,
+  plugins: [react(), chunkSplitPlugin()],
 })
