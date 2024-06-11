eb5c1f81776d02d31fb0ebb075542f16119c7838
Luca Donadello <45766869+LucaDonadello@users.noreply.github.com>
2024-06-11 12:08:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import chunkSplitPlugin from 'your-module-path/chunkSplitPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), chunkSplitPlugin()],
+  plugins: [react()],
+  build: { chunkSizeWarningLimit: 1600, }
 })
